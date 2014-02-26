1127589249ac1fbae1c24faaf30e2d2fe38f0b81
Hirshol Pheir <hhp@zuma.com>
2014-02-26 14:13:46 -0400

    dashboard page enhancement

--- app/views/dashboard/home.blade.php
+++ app/views/dashboard/home.blade.php
@@ -1,10 +1,75 @@
+<style>
+.ui-body {
+	height: 200px;
+}
 
-<h3 class="ui-bar ui-bar-a ui-corner-all">Scraper Administration</h3>
-    <div class="ui-body">
-        <p>The Scraper Administration uses the Mids Sheet information to scrape processor websites 
-        for charge back data.
-        </p>
-          <p><a class="ui-btn ui-btn-inline" href="/scraper/">Go</a></p>
+.ui-block-a,.ui-block-b,.ui-block-c,.ui-block-d {
+	padding: 0px 2px 4px 2px;
+}
+
+.go {
+	position: absolute;
+	bottom: 0;
+}
+</style>
+
+<div class="ui-grid-c ">
+
+    <div class="ui-block-a">
+        <div class="ui-body ui-body-a ">
+            <h3>User Admin</h3>
+            <p>The User Admin tool manages users of this application.</p>
+            <p>
+                <a class="ui-btn ui-btn-inline go" href="/scraper/">Go</a>
+            </p>
+        </div>
     </div>
 
+    <div class="ui-block-b">
+        <div class="ui-body ui-body-a ">
+            <img src="./images/secret_smile.png" width="32" height="32" style="float: left; margin-right: 10px; margin-top: 10px; ">
+            <h3>Scraper Admin</h3>
+            <p>The Scraper Administration uses the Mids Sheet
+                information to scrape processor websites for charge back
+                data.</p>
+            <p>
+                <a class="ui-btn ui-btn-inline go" href="/scraper/">Go</a>
+            </p>
+        </div>
+    </div>
+
+    <div class="ui-block-c">
+        <div class="ui-body ui-body-a ">
+            <h3>Metrics</h3>
+            <p>x</p>
+        </div>
+    </div>
+
+    <div class="ui-block-d">
+        <div class="ui-body ui-body-a ">
+            <h3>Accounting</h3>
+            <p>x</p>
+        </div>
+    </div>
+
+    <div class="ui-block-a">
+        <div class="ui-body ui-body-a ">
+            <h3>Customers</h3>
+            <p>x</p>
+        </div>
+    </div>
     
+    <div class="ui-block-b">
+        <div class="ui-body ui-body-a ">
+            <h3>TODOs</h3>
+            <p>My list of TODOs</p>
+        </div>
+    </div>
+    
+    
+    
+</div>
+<!-- /grid-c -->
+
+
+
--- app/views/scraper/account.blade.php
+++ app/views/scraper/account.blade.php
@@ -1,20 +1,22 @@
 <span style="float: right;"> 
     <a href="#" class="ui-btn ui-btn-inline ui-mini btn-start-scrape" data-account-id="{{ $account->id }}">Start Scrape</a> 
-    <a href="#" class="ui-btn ui-btn-inline ui-mini">Scrape Logs</a> 
+    <a href="#" class="ui-btn ui-btn-inline ui-mini">Edit</a>
     <a href="#" class="ui-btn ui-btn-inline ui-mini">Delete</a>
 </span>
 <table>
     <tbody>
         <tr>
-            <td class="ui-bold">Login</td>
+            <td class="ui-bold" style="text-align: right; ">Login</td>
             <td><?=$account->uid?></td>
         </tr>
         <tr>
-            <td>Password</td>
+            <td style="text-align: right; ">Password</td>
             <td><?=$account->password?></td>
         </tr>
+        
+        
         <tr>
-            <td>Files:</td>
+            <td style="vertical-align: top; text-align: right;">Scrapes</td>
             <td>
                 <?php foreach($scrapeFiles as $sf): ?>
                 <a href="<?='/scraper/download-scrape/?file='.$filepath.'/'.$sf; ?>" target="_blank"><?=$sf?></a><br>
